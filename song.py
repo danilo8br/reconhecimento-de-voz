e3be3d71842881ae6c053db868819edc2c539e73
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2021-01-27 15:01:20 -0300

    Add files via upload

---
+++ song.py
@@ -0,0 +1,16 @@
+# Importando a biblioteca 
+import speech_recognition as sr
+# Criando uma instância da classe "Recognizer"
+r = sr.Recognizer()
+# Utilizando Microfone
+with sr.Microphone() as fonte:
+    # Eliminando ruidos
+    r.adjust_for_ambient_noise(fonte, duration=3) 
+    print('Diga algo: ')
+    # Capturando o audio
+    audio = r.listen(fonte)
+    try:
+        # Transoformando o audio em texto
+        print('Você disse: ' + r.recognize_google(audio, language='pt-BR')) 
+    except:
+        print('Não foi possivel compreender o áudio')
